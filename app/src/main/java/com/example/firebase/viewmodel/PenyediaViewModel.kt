a42357eea60be9c6c630241d5daac771742c5844
Cruelasss <irula5198@gmail.com>
2026-01-08 08:43:21 +0700

    Implementasi DetailViewModel pada PenyediaViewModel
    • Menambahkan import `createSavedStateHandle` untuk mendukung pengelolaan state pada ViewModel.
    • Menambahkan initializer untuk `DetailViewModel` ke dalam `viewModelFactory`.
    • Menginjeksikan `SavedStateHandle` dan `repositorySiswa` ke dalam instance `DetailViewModel`.

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -20,6 +21,15 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
