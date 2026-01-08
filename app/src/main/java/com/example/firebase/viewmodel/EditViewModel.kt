74120018156a15c0e788d0c0dde5d66e0b4052d7
Cruelasss <irula5198@gmail.com>
2026-01-08 08:13:49 +0700

    Implementasi EditViewModel untuk manajemen state pembaruan data siswa
    • Mengubah `EditViewModel` menjadi subclass dari `ViewModel`.
    • Menambahkan `SavedStateHandle` dan `RepositorySiswa` sebagai dependensi konstruktor.
    • Mendeklarasikan properti `uiStateSiswa` menggunakan `mutableStateOf` dengan visibility setter privat untuk mengelola state UI pada layar edit.

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -1,4 +1,10 @@
 package com.example.firebase.viewmodel
 
 class EditViewModel {
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
 }
