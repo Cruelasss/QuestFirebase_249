568b5d79cee03c55821fdd93c73498eba3cf1f3a
Cruelasss <irula5198@gmail.com>
2026-01-08 08:10:42 +0700

    Implementasi fungsi hapus data pada FirebaseRepositorySiswa
    • Menambahkan implementasi metode `hapusSatuSiswa` dalam class `FirebaseRepositorySiswa`.
    • Menggunakan query Firestore untuk mencari dokumen berdasarkan field "id" dan menghapusnya secara asinkron menggunakan `await()`.

--- app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -69,5 +69,13 @@ class FirebaseRepositorySiswa : RepositorySiswa {
                 "nama" to siswa.nama,
                 "alamat" to siswa.alamat,
                 "telpon" to siswa.telpon
+            )
+        ).await()
+    }
+
+    override suspend fun hapusSatuSiswa(id: Long) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).delete().await()
     }
 }
